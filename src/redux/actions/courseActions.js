72b665772b70ae38381d54c05d2c252b6a195a17
mike-egner <egg-meister@hotmail.com>
2019-04-12 21:51:35 +0200

    replaced hardcoded actionCreator types with consts

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,3 +1,5 @@
+import * as types from "./actionTypes";
+
 export function createCourse(course) {
-  return { type: "CREATE_COURSE", course }; //object shorthand syntax used
+  return { type: types.CREATE_COURSE, course }; //object shorthand syntax used
 }
