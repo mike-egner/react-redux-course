2750710c0cf0844af2cecdfe0a49395ddc3d82e7
Mike Egner <egg-meister@hotmail.com>
2019-04-28 22:03:43 +0200

    added courses total to header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Header = () => {
+const Header = ({ coursesTotal }) => {
   const activeStyle = { color: "#F15B2A" };
+  debugger;
   return (
-    <nav>
-      <NavLink to="/" activeStyle={activeStyle} exact>
-        Home
-      </NavLink>
-      {" | "}
-      <NavLink to="/courses" activeStyle={activeStyle}>
-        Courses
-      </NavLink>
-      {" | "}
-      <NavLink to="/about" activeStyle={activeStyle}>
-        About
-      </NavLink>
+    <nav className="navbar navbar-light bg-light py-3">
+      <div>
+        <NavLink to="/" activeStyle={activeStyle} exact>
+          Home
+        </NavLink>
+        {" | "}
+        <NavLink to="/courses" activeStyle={activeStyle}>
+          Courses
+        </NavLink>
+        {" | "}
+        <NavLink to="/about" activeStyle={activeStyle}>
+          About
+        </NavLink>
+      </div>
+      <span>
+        {coursesTotal === 0
+          ? "Click Courses to Load Courses"
+          : "Courses: " + coursesTotal}
+      </span>
     </nav>
   );
 };
 
-export default Header;
+function mapStateToProps(state) {
+  const coursesTotal = state.courses.length;
+  return { coursesTotal };
+}
+
+export default connect(mapStateToProps)(Header);
